edf86781c0f40a20f04471592f87773dc06ba153
tash02 <tash.brinkley@gmail.com>
2018-07-13 17:20:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector (selector) {
   return document.querySelector(selector)
 }
 function nestedTarget () {
-  return document.querySelector (selector.target)
+  return document.querySelector (#selector.target)
 }
