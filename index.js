de69e16e9a0b88c1702baf20f8b63800ea1f8d19
tash02 <tash.brinkley@gmail.com>
2018-07-13 17:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function nestedTarget () {
 function deepestChild () {
   return document.querySelector("#grand-node div div div div")
 }
-function increaseRankBy(){
-  
-}
-
+function increaseRankBy(n){
+ const lis = document.querySelectorAll('ul.ranked-list')
+ for (let i=0; i < lis.length; i++) {
+   lis[i].innerHTML = (i+1).toString()
+ }
+ return document.querySelector(".ranked-list ")
